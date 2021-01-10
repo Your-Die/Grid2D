8f636835533fe795fd4c3c03cbcdc843d5383261
Jordy <jordy_van_opstal@live.nl>
2021-01-10 01:16:02 +0100

    Add EmptyGridGenerator

---
+++ PCG/Scripts/EmptyGridGenerator.cs
@@ -0,0 +1,16 @@
+﻿namespace Chinchillada.Generation.Grid
+{
+    using System;
+    using Chinchillada.Grid;
+    using UnityEngine;
+
+    [Serializable]
+    public class EmptyGridGenerator : GeneratorBase<Grid2D>
+    {
+        [SerializeField] private int width;
+
+        [SerializeField] private int height;
+        
+        protected override Grid2D GenerateInternal() => new Grid2D(this.width, this.height);
+    }
+}
