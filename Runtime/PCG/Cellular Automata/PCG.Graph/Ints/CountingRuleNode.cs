fbfb402209a968711e9a6379d82d5b0c63d00750
Jordy van Opstal <jordy_van_opstal@live.nl>
2024-05-02 10:12:24 +0200

    Deconstruct parameters of count constraint in CountingRuleNode

--- Runtime/PCG/Cellular Automata/PCG.Graph/Ints/CountingRuleNode.cs
+++ Runtime/PCG/Cellular Automata/PCG.Graph/Ints/CountingRuleNode.cs
@@ -11,13 +11,15 @@ namespace Chinchillada.Grid.PCGraphs.Ints
         [Input, ShowAsDrawer] public int resultValue;
         [Input, ShowAsDrawer] public int targetValue;
 
+        [Input, ShowAsDrawer] public int constraintValue = 3;
+        [Input, ShowAsDrawer] public CountConstraint.ComparisonOperator constraintOperator;
+
         [Input] public INeighborhoodFactory neighborhood = new Diagonal.Factory();
-        
-        [ShowInInspector] public CountConstraint Constraint;
-        
+
         public override CountingRule<int> Generate()
         {
-            return new CountingRule<int>(this.resultValue, this.targetValue, this.Constraint, this.neighborhood);
+            var constraint = new CountConstraint(this.constraintOperator, this.constraintValue);
+            return new CountingRule<int>(this.resultValue, this.targetValue, constraint, this.neighborhood);
         }
     }
 }
